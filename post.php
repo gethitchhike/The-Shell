2a0100cdef1eef5f548bbceaa621d7f9f99613dd
squarerootfury <fury224@googlemail.com>
2016-10-15 23:06:29 +0200

    + CSS fixes post display

--- post.php
+++ post.php
@@ -41,25 +41,27 @@
 			}
 		?>
 	</section>
-	<section class="author">
-		<h4><?php echo $post->Author->Name;?></h4>
-		<p><?php echo $post->Author->About;?></p>
-	</section>
+	<div class="post-post-meta">
+		<section class="author">
+			<h4><?php echo $post->Author->Name;?></h4>
+			<p><?php echo $post->Author->About;?></p>
+		</section>
 
-	<section class="share">
-		<?php
-			$href = urlencode($blog->URL."?/post/".$post->WebFilename."/");
-		?>
-		<a class="icon-twitter" href="http://twitter.com/share?text=<?php echo $post->Title;?>&amp;url=<?php echo $href;?>" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;">
-			<span class="hidden">Twitter</span>
-		</a>
-		<a class="icon-facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $href;?>" onclick="window.open(this.href, 'facebook-share','width=580,height=296');return false;">
-			<span class="hidden">Facebook</span>
-		</a>
-		<a class="icon-google-plus" href="https://plus.google.com/share?url=<?php echo $href;?>" onclick="window.open(this.href, 'google-plus-share', 'width=490,height=530');return false;">
-			<span class="hidden">Google+</span>
-		</a>
-	</section>
+		<section class="share">
+			<?php
+				$href = urlencode($blog->URL."?/post/".$post->WebFilename."/");
+			?>
+			<a class="icon-twitter" href="http://twitter.com/share?text=<?php echo $post->Title;?>&amp;url=<?php echo $href;?>" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;">
+				<span class="hidden">Twitter</span>
+			</a>
+			<a class="icon-facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $href;?>" onclick="window.open(this.href, 'facebook-share','width=580,height=296');return false;">
+				<span class="hidden">Facebook</span>
+			</a>
+			<a class="icon-google-plus" href="https://plus.google.com/share?url=<?php echo $href;?>" onclick="window.open(this.href, 'google-plus-share', 'width=490,height=530');return false;">
+				<span class="hidden">Google+</span>
+			</a>
+		</section>
+	</div>
 	</footer>
 </article>
 </main>
